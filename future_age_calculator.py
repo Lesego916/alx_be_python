5dd4a6debabd155bb14dcb0b576c7edb534fe669
Lesego916 <segobuyisile@gmail.com>
2025-08-28 15:30:13 +0200

    python_introduction

---
+++ future_age_calculator.py
@@ -0,0 +1,8 @@
+# Prompt user for input
+current_age = int(input("How old are you? "))
+
+# Future age in 2050 (27 years after 2023)
+future_age = current_age + 27
+
+# Output
+print(f"In 2050, you will be {future_age} years old")
---
+++ rectangle_area.py
@@ -0,0 +1,9 @@
+# Variables
+length = 10
+width = 5
+
+# Formula: Area = length × width
+area = length * width
+
+# Output
+print(f"The area of the rectangle is: {area}")
